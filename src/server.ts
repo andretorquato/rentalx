d4ed7588658cbc64cf001a654e7cf3931a00345c
André Torquato <andretorquato@alu.ufc.br>
2021-08-12 23:32:59 -0300

    use debug

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.listen("3333", () => {
+const courses = [];
+
+app.get("/", (request, response) => {
+  return response.status(200).json({ message: "Rentalx start" });
+});
+
+app.post("/courses", (request, response) => {
+  const course = request.body;
+  const { name, description } = course;
+  courses.push(course);
+  return response.json({ name, description });
+});
+
+app.listen(3333, () => {
   console.log("Server is running on port 3333");
 });
