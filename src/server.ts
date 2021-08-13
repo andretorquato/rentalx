975206e90b4f9e64940df5415c2940530640a2cb
André Torquato <andretorquato@alu.ufc.br>
2021-08-13 17:48:37 -0300

    feat: categories routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,14 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 app.use(express.json());
-
-const courses = [];
 
 app.get("/", (request, response) => {
   return response.status(200).json({ message: "Rentalx start" });
 });
-
-app.post("/courses", (request, response) => {
-  const course = request.body;
-  const { name, description } = course;
-  courses.push(course);
-  return response.json({ name, description });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
