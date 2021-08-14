b95420e0a16df00ec989a0c581ff58c7c24fd148
André Torquato <andretorquato@alu.ufc.br>
2021-08-14 19:14:13 -0300

    create uuid

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ app.use(express.json());
 app.get("/", (request, response) => {
   return response.status(200).json({ message: "Rentalx start" });
 });
-app.use(categoriesRoutes);
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
