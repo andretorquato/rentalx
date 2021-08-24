6d75f2794f1159bb7d4097e596f52feedfb205f3
André Torquato <andretorquato@alu.ufc.br>
2021-08-23 23:56:47 -0300

    :recycle:refact: routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,11 @@
 import { Router } from "express";
 
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationsController } from "../modules/cars/useCases/createSpecifications";
 
 const specificationsRouter = Router();
-const specificationsRepository = new SpecificationsRepository();
 
 specificationsRouter.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-  createSpecificationService.execute({ name, description });
-  response.status(201).send();
+  return createSpecificationsController.handle(request, response);
 });
 
 export { specificationsRouter };
