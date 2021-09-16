78fc7eb35aaa77fac35c7d44b00bc4c713d8ed75
André Torquato <andretorquato@alu.ufc.br>
2021-09-16 10:14:29 -0300

    test bug pg

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,7 +7,6 @@ interface IOptions {
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
   newOptions.host = "database";
-
   createConnection({
     ...options,
   });
