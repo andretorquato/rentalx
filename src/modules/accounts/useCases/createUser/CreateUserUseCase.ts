cf53ab29f9045ed7d7985fcc7df59b4eb007ba1c
André Torquato <andretorquato@alu.ufc.br>
2021-09-28 22:46:01 -0300

    Create Users migration

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    driver_license,
+    password,
+    username,
+  }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      email,
+      password,
+      username,
+      driver_license,
+    });
+  }
+}
+export { CreateUserUseCase };
