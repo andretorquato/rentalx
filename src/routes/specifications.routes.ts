b34e5c4af202f8cf9f477d06c809a9b86d4be5a9
André Torquato <andretorquato@alu.ufc.br>
2021-10-03 17:42:27 -0300

    create authenticate middleware

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 import { CreateSpecificationsController } from "../modules/cars/useCases/createSpecifications/CreateSpecificationsController";
 
 const specificationsRouter = Router();
 
 const createSpecificationsController = new CreateSpecificationsController();
+specificationsRouter.use(ensureAuthenticated);
 specificationsRouter.post("/", createSpecificationsController.handle);
 
 export { specificationsRouter };
