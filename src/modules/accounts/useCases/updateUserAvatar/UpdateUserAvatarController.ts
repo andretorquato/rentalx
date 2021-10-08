f75a259cecd2758fb3d0e96bb0f2fdb666ec437f
André Torquato <andretorquato@alu.ufc.br>
2021-10-08 16:26:23 -0300

    create upload controller

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUserCase";
 
 class UpdateUserAvatarController {
-  async handle(request: Request, response: Response) { }
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id } = request.user;
+
+    const avatar_file = null;
+
+    const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+
+    await updateUserAvatarUseCase.execute({
+      user_id: id,
+      avatar_url: avatar_file,
+    });
+    return response.status(200).send();
+  }
 }
 
 export { UpdateUserAvatarController };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
-
+const updateUserAvatarController = new UpdateUserAvatarController();
 usersRoutes.post("/", createUserController.handle);
+
+usersRoutes.patch(
+  "/import",
+  upload.single("file"),
+  updateUserAvatarController.handle
+);
 
 export { usersRoutes };
