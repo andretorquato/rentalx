2925567562624a2a076c782687d6890756bb8135
André Torquato <andretorquato@alu.ufc.br>
2021-10-14 21:53:10 -0300

    delete cache images

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUserCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUserCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -15,6 +16,9 @@ class UpdateUserAvatarUseCase {
 
   async execute({ avatar_url, user_id }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
     user.avatar = avatar_url;
 
     await this.usersRepository.create(user);
