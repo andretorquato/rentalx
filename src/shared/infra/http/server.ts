11ac6e860e9a9095d1f9e0d47b34485877bda568
André Torquato <andretorquato@alu.ufc.br>
2021-11-26 17:08:29 -0300

    refactor

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,11 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
-import "../../../database";
+import "@shared/infra/typeorm";
 
 import "../../container";
 
-import { AppError } from "@errors/AppError";
+import { AppError } from "@shared/errors/AppError";
 
 import swaggerFile from "../../../../swagger.json";
 import { router } from "./routes";
